d1d8d7d7c25ed2a41d8e8d34d47efb4c69698c6a
Mykyta Opaniuk <mykyta@opanyuk.com>
2019-12-02 10:57:12 +0200

    Lab 1: Modified parsing method + added layouts for templates
    Checking for includeing of *.go files in project and module (directory).
    Templates will be used in generated HTML-format documents.
    Modified README file.
    
    Signed-off-by: Mykyta Opaniuk <mykyta@opanyuk.com>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Metaprogramming_lab_1
 Design and development of software documentation generators
+
+USAGE: python goland_doc.py [argument] [value]
+    List of arguments:
+       	--version (-v): Path to directory for parsing.
+       	--directory (-d) <path-to-directory> : Path to directory for parsing.
+		Not recursive parsing.
+       	--project (-p) <path-to-project> : Path to project for parsing.
+		Recursive parsing (find *.go files using os.walk()).
+       	--file (-f) <path-to-file> : Path to file for parsing.
--- golang_doc.py
+++ golang_doc.py
@@ -1,23 +1,48 @@
 import argparse
+import os
+import glob
+from typing import List, Union
 
-class GolangDocGen:
-    VERSION = "pre0.1"
-
+VERSION = "pre0.1"
 
 parser = argparse.ArgumentParser(description='Getting arguments for parsing Golang files/projects.')
 
-parser.add_argument("--version",  "-v", action='version', version=GolangDocGen.VERSION)
-parser.add_argument("--directory", "-d", help="Path to directory for parsing.")
-parser.add_argument("--project", "-p", help="Path to project for parsing.")
-parser.add_argument("--file", "-f",  help="Path to file for parsing.")
+parser.add_argument("--version",  "-v", action='version', version=VERSION)
+parser.add_argument("--directory", "-d", help="Path to directory of *.go files")
+parser.add_argument("--project", "-p", help="Path to project of *.go files")
+parser.add_argument("--file", "-f", help="Path to *.go file")
 
 args = parser.parse_args()
 
 if args.project is not None:
-    print(args.project)
+    if os.path.isdir(args.project):
+        print(args.project)
+        all_files = []
+        for d,dirs, files in os.walk(args.project):
+            for f in files:
+                all_files.append(f)
+        golang_files = list(filter(lambda x: x.endswith('.py'), all_files))
+        if len(golang_files) > 0:
+            print(golang_files)
+        else:
+            print("Failed! Project doesn't contain *.go files.")
+    else:
+        print("Failed! Argument isn't a path.")
 
 elif args.directory is not None:
-    print(args.directory)
+    if os.path.isdir(args.directory):
+        print(args.directory)
+        files = os.listdir(args.directory)
+        golang_files = list(filter(lambda x: x.endswith('.py'), files))
+        if len(golang_files) > 0:
+            print(golang_files)
+        else:
+            print("Failed! Directory doesn't contain *.go files.")
+    else:
+        print("Failed! Argument isn't a directory.")
 
 elif args.file is not None:
-    print(args.file)
+    if os.path.isfile(args.file):
+        print(args.file)
+    else:
+        print("Failed! Argument isn't a file.")
